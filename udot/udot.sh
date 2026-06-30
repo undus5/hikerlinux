dbc1c4a6be1b20efa1c3851bb8ea444f80823ad1
undus5 <mail@undus.net>
2026-06-30 17:07:31 +0800

    udot: add bashrc

--- udot/udot.sh
+++ udot/udot.sh
@@ -6,22 +6,24 @@ tilde_path() { echo "$1" | sed "s#$(realpath ~)#~#"; }
 
 [[ "$EUID" != "0" ]] || errf "==> abort for super user"
 
-self_dir=$(dirname $(realpath ${BASH_SOURCE[0]}))
-src_dir=$self_dir
-conf_dir=~/.config
-mkdir -p $conf_dir
+SELF_DIR=$(dirname $(realpath ${BASH_SOURCE[0]}))
+SRC_DIR=$SELF_DIR
+CONF_DIR=~/.config
+mkdir -p $CONF_DIR
 
 test_names() {
    local names=()
    if [[ "$1" == "all" ]]; then
-      mapfile -t names < <(find $src_dir -mindepth 1 -maxdepth 1 -type d \
+      mapfile -t names < <(find $SRC_DIR -mindepth 1 -maxdepth 1 -type d \
          ! -name ".git" \
          ! -name "kanshi" \
          -exec basename '{}' \;)
+      names+=( "bashrc" )
    else
       for n in "$@"; do
          n=${n%/}
-         [[ "$n" == "rime" || -d ${src_dir}/${n} ]] || errf "name not found: $n"
+         [[ "$n" == "rime" || "$n" == "bashrc" || -d ${SRC_DIR}/${n} ]] \
+            || errf "name not found: $n"
          [[ "$n" != "kanshi" ]] && names+=("$n")
       done
    fi
@@ -30,54 +32,66 @@ test_names() {
 
 install_config() {
    local n="$1"
-   if [[ -d ${conf_dir}/${n} ]]; then
-      rm -rf ${conf_dir}/${n}
-      echo "==> removed '$(tilde_path ${conf_dir}/${n})'"
+   if [[ -d ${CONF_DIR}/${n} ]]; then
+      rm -rf ${CONF_DIR}/${n}
+      echo "==> removed '$(tilde_path ${CONF_DIR}/${n})'"
    fi
-   cp -rf ${src_dir}/${n} ${conf_dir}/${n}
-   echo "==> installed '$(tilde_path ${conf_dir}/${n})'"
+   cp -rf ${SRC_DIR}/${n} ${CONF_DIR}/${n}
+   echo "==> installed '$(tilde_path ${CONF_DIR}/${n})'"
 }
 
 merge_config() {
    local n="$1"
-   if [[ -d ${conf_dir}/${n} ]]; then
-      rm -rf ${src_dir}/${n}
-      cp -rf ${conf_dir}/${n} ${src_dir}/${n}
-      echo "==> merged '$(tilde_path ${src_dir}/${n})'"
+   if [[ -d ${CONF_DIR}/${n} ]]; then
+      rm -rf ${SRC_DIR}/${n}
+      cp -rf ${CONF_DIR}/${n} ${SRC_DIR}/${n}
+      echo "==> merged '$(tilde_path ${SRC_DIR}/${n})'"
    fi
 }
 
-install_lite_xl_config() {
-   mkdir -p ${conf_dir}/lite-xl
+install_lite_xl() {
+   mkdir -p ${CONF_DIR}/lite-xl
    local path="lite-xl/init.lua"
-   cp -f ${src_dir}/${path} ${conf_dir}/${path}
-   echo "==> installed '$(tilde_path ${conf_dir}/${path})'"
+   cp -f ${SRC_DIR}/${path} ${CONF_DIR}/${path}
+   echo "==> installed '$(tilde_path ${CONF_DIR}/${path})'"
 }
 
-merge_lite_xl_config() {
+merge_lite_xl() {
    local path="lite-xl/init.lua"
-   cp -f ${conf_dir}/${path} ${src_dir}/${path}
-   echo "==> merged '$(tilde_path ${src_dir}/${path})'"
+   cp -f ${CONF_DIR}/${path} ${SRC_DIR}/${path}
+   echo "==> merged '$(tilde_path ${SRC_DIR}/${path})'"
 }
 
 install_rime_wubi86s() {
    local rime_dir=~/.local/share/fcitx5/rime
    mkdir -p $rime_dir
-   cp -f ${src_dir}/rime-wubi86s/*.yaml ${rime_dir}/
+   cp -f ${SRC_DIR}/rime-wubi86s/*.yaml ${rime_dir}/
    echo "==> installed '$(tilde_path ${rime_dir})/*.yaml'"
 }
 
 merge_rime_wubi86s() {
    local rime_dir=~/.local/share/fcitx5/rime
-   mapfile -t names < <(find ${src_dir}/rime-wubi86s -type f -name "*.yaml" \
+   mapfile -t names < <(find ${SRC_DIR}/rime-wubi86s -type f -name "*.yaml" \
       -exec basename '{}' \;)
    for n in ${names[@]}; do
       if [[ -f ${rime_dir}/${n} ]]; then
-         cp -f ${rime_dir}/${n} ${src_dir}/rime-wubi86s/ 
+         cp -f ${rime_dir}/${n} ${SRC_DIR}/rime-wubi86s/ 
       fi
    done
-   echo "==> merged '$(tilde_path ${src_dir}/rime-wubi86s)/*.yaml'"
+   echo "==> merged '$(tilde_path ${SRC_DIR}/rime-wubi86s)/*.yaml'"
 }
+
+install_rime() { install_rime_wubi86s; }
+merge_rime() { merge_rime_wubi86s; }
+
+install_bashrc() {
+   if ! grep -q 'sway/bins' ~/.bashrc; then
+      cat ${SRC_DIR}/_bashrc >> ~/.bashrc
+      echo "==> appended '$(tilde_path ${SRC_DIR}/_bashrc)' to '~/.bashrc'"
+   fi
+}
+
+merge_bashrc() { return; }
 
 case "$1" in
    install)
@@ -86,11 +100,8 @@ case "$1" in
       [[ -n "$names" ]] || errf "empty names"
       for n in ${names[@]}; do
          case "$n" in
-            lite-xl)
-               install_lite_xl_config
-               ;;
-            rime-wubi86s|rime)
-               install_rime_wubi86s
+            lite-xl|rime-wubi86s|rime|bashrc)
+               install_${n}
                ;;
             *)
                install_config $n
@@ -104,11 +115,8 @@ case "$1" in
       [[ -n "$names" ]] || errf "empty names"
       for n in ${names[@]}; do
          case "$n" in
-            lite-xl)
-               merge_lite_xl_config
-               ;;
-            rime-wubi86s|rime)
-               merge_rime_wubi86s
+            lite-xl|rime-wubi86s|rime|bashrc)
+               merge_${n}
                ;;
             *)
                merge_config $n
